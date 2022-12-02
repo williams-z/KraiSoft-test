f614c2ef5510b9055bb84a553726f34c3dfea0c7
williams-z <williams.z.bis@gmail.com>
2022-12-02 18:21:41 +1300

    added vaguely working script

---
+++ app.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+  console.log('load');
+  
+  const parent = document.querySelector('.parent')
+  const parentBox = parent.getBoundingClientRect()
+
+  const draggable = document.getElementById('drag-box')
+  const dragBox = draggable.getBoundingClientRect()
+
+  let moving = false
+
+  function mouseDown(e) {
+    e.preventDefault()
+    moving = true
+    console.log('click');
+  }
+
+  function mouseUp(e) {
+    e.preventDefault()
+    moving = false
+    console.log('click off');
+  }
+
+  function onDrag(e) {
+    e.preventDefault()
+    console.log('moving');
+
+
+    if ( moving ) {
+      if ((e.clientX >= parentBox.left &&(e.clientX + dragBox.width <= parentBox.right)) && 
+        (e.clientY >= parentBox.top && (e.clientY+dragBox.height <= parentBox.bottom))
+        ) { 
+          draggable.style.left = `${e.clientX}px`
+          draggable.style.top = `${e.clientY}px`
+      } else {
+        if (e.clientX + dragBox.width >= parentBox.right) {
+          draggable.style.left = `${parentBox.right - dragBox.width}px`
+        }
+        if (e.clientY + dragBox.height >= parentBox.bottom) {
+          draggable.style.top = `${parentBox.bottom - dragBox.height}px`
+        }
+      }  
+    }
+  }
+
+  document.addEventListener('mousedown', mouseDown)
+  document.addEventListener('mousemove', onDrag)
+  document.addEventListener('mouseup', mouseUp)
+}
+
+
